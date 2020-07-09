da046f8bdf0c8cb5686e27549fd416c21a1a41fd
AchmadThoriq <dantethoriq80@gmail.com>
2020-07-09 11:17:43 +0700

    update package editor

--- resources/views/text-editor.blade.php
+++ resources/views/text-editor.blade.php
@@ -1,6 +1,6 @@
 <!-- uji coba package editor -->
 
-
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 <div class= "ml-3 mt-3">
 <form action="/"  method = "POST">
 @csrf
@@ -8,12 +8,13 @@
 <div class="form-group">
   <label for="judul">Judul :</label>
   <input type="text" class="form-control" id="judul" placeholder="Masukan Judul" name="judul" >
+  <textarea name="judul" class="form-control my-editor">{!! old('judul', $judul ?? '') !!}</textarea>
 </div>
 
 <div class="form-group">
   <label for="isi">Isi :</label>
-  <textarea id="konten" class="form-control" name="konten" rows="10" cols="50"></textarea>
-  <!-- <input type="text" class="form-control" id="isi" placeholder="Masukan Isi" name="isi" > -->
+  <input type="text" class="form-control" id="isi" placeholder="Masukan Isi" name="isi" >
+  <textarea name="isi" class="form-control my-editor">{!! old('isi', $isi ?? '') !!}</textarea>
 </div>
 
 <div class="form-group">
@@ -32,13 +33,41 @@
 
 </div>
 
-
-
-<script src="{{asset('/ckeditor/ckeditor.js')}}"></script>
 <script>
-  var konten = document.getElementById("konten");
-    CKEDITOR.replace(konten,{
-    language:'en-gb'
-  });
-  CKEDITOR.config.allowedContent = true;
+  var editor_config = {
+    path_absolute : "/",
+    selector: "textarea.my-editor",
+    plugins: [
+      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+      "searchreplace wordcount visualblocks visualchars code fullscreen",
+      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "emoticons template paste textcolor colorpicker textpattern"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+    relative_urls: false,
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
 </script>
+
+
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/coba', function () {
+Route::get('/coba', function () { // menampilkan package editor
     return view('text-editor');
 });
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    \UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
